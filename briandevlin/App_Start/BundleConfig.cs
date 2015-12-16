2d1829425eda246f9e14f2605b87c1fc89b06463
briandevlin <brian.devlin@gmail.com>
2015-12-16 08:43:25 -0500

    laptop commit from VS

---
+++ briandevlin/App_Start/BundleConfig.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading;
+using System.IO;
+using System.Web.Optimization;
+using System.Web;
+
+
+namespace briandevlin.App_Start
+{
+    public class BundleConfig
+    {
+
+        public static string AdminAppDir = "app";
+        public static string BundleOutputFile = "scripts\\briandevlin.bundled.js";
+        public static string ScriptsBundleVirtualPath = "~/bundles/appScripts";
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-2.1.4.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                      "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                   "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularJS").Include(
+           "~/Scripts/angular.min.js",            
+           "~/Scripts/angular-ui-router.min.js",
+           "~/Scripts/angular-ui/ui-bootstrap-tpls.min.js",
+            "~/Scripts/angular-ui/ui-bootstrap.min.js",
+           "~/Scripts/ui-grid.min.js",                     
+           "~/Scripts/angular-animate.min.js"           
+           ));
+
+            bundles.Add(new ScriptBundle("~/bundles/widgits").Include(
+           "~/Scripts/lodash.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                     "~/Content/bootstrap.css",
+                     "~/Content/bootstrap-theme.min.css",                  
+                     "~/Content/ui-grid.min.css",
+                     "~/Content/animate.css",
+                     "~/Content/site.css"));
+
+            AddAppBundles(bundles);
+
+        }
+
+        private static void AddAppBundles(BundleCollection bundles)
+        {
+            var scriptBundle = new ScriptBundle(ScriptsBundleVirtualPath);
+#if (DEBUG && LOCAL)
+            var adminAppDirFullPath = Path.Combine(HttpRuntime.AppDomainAppPath, AdminAppDir);
+
+            // Create the .NET bundle, then use that list to generate an actual bundle.js file
+            if (Directory.Exists(adminAppDirFullPath))
+            {
+                scriptBundle.Include(
+                    // Order matters, so get the core app setup first
+                    string.Format("~/{0}/app.core.module.js", AdminAppDir),
+                    string.Format("~/{0}/app.module.js", AdminAppDir))
+                    // then get any other top level js files
+                    .IncludeDirectory(string.Format("~/{0}", AdminAppDir), "*.js", false)
+                    // then get all nested module js files
+                    .IncludeDirectory(string.Format("~/{0}", AdminAppDir), "*-module.js", true)
+                    // finally, get all the other js files
+                    .IncludeDirectory(string.Format("~/{0}", AdminAppDir), "*.js", true);
+
+                bundles.Add(scriptBundle);
+
+                // Generate a new bundle script.  This should only be done on a local box as running from the build server causes issues.
+                //GenerateBundleJS(scriptBundle, bundles);
+            }
+#elif (DEBUG && !LOCAL)
+            scriptBundle.Include(string.Format("~/{0}/briandevlin.bundled.js", "scripts"));
+            bundles.Add(scriptBundle);
+#elif !DEBUG
+            scriptBundle.Include(string.Format("~/{0}/briandevlin.bundled.min.js", "scripts"));
+            bundles.Add(scriptBundle);
+#endif
+        }
+    }
+}
---
+++ briandevlin/app/app.core.module.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.core', [
+          
+            'ui.router',           
+            'ui.bootstrap',        
+            'ui.grid',              
+            'ui.grid.selection',
+            'ngAnimate'   
+        ]);
+})();
